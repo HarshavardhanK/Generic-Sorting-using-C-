7b6e1ee0098ae9f6f2fe363dca72a7213a855309
HarshavardhanK <harshavardhan816@gmail.com>
2016-08-26 21:16:41 +0530

    Initial Commit

---
+++ Generic Sorting using C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Generic Sorting using C++
+//
+//  Created by Harshavardhan K K on 26/08/16.
+//  Copyright © 2016 Harshavardhan K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
